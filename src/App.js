15e2ad88a546c27e01cea17c88a8a80b00960f99
Pranjael Bagaria <pranjael2719@gmail.com>
2021-07-17 17:30:41 +0530

    Code Editor Task 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  import React, { Component } from 'react';
 
-export default App;
+  export default class App extends Component {
+    render() {
+      return (
+        <div style = {{textAlign: 'center'}}>
+          <h3 style = {{fontSize: 40}}>Code Editor</h3>
+          <label for="language" >Language: </label>
+          <input type="radio" name="id"></input>
+          <label style={{margin: 5}}>HTML</label>
+          <input type="radio" name="id"></input>
+          <label style={{margin: 5}}>CSS</label>
+          <input type="radio" name="id"></input>
+          <label style={{margin: 5}}>JavaScript</label>
+          <div><textarea  style={{ height: 200, width: 700 }}></textarea></div>
+          <button>Execute</button>
+          <button style={{margin: 5}}>View Result</button>
+        </div>
+      );
+    }
+  }
